471ff68cda00173498dda09e3df4c650e9983e83
Julius Suominen <julius.suominen@gmail.com>
2016-10-09 18:32:25 +0300

    Riot --> jQuery

--- src/public/js/kaljasakot.js
+++ src/public/js/kaljasakot.js
@@ -1,14 +1,28 @@
-$(document).ready(function(){
-var count = 0;
+$(function() {
 
-$(".kaljasakot-add").click(function(){
-	count++;
-	$(".kaljasakot-sum").val(count);
+	var count = $(".kaljasakko-sum").val();
+
+	$(".kaljasakko-add").click(function(){
+
+		count++;
+		$(".kaljasakko-sum").val(count);
+	
+	});
+
+	$(".kaljasakko-remove").click(function(){
+
+		count--;
+		$(".kaljasakko-sum").val(count);
+	
+	});
+
+	$(".kaljasakko-name").change(function(){
+		
+		var num = $(".kaljasakko").length;
+		console.log(num);
+		newNum  = new Number(num + 1);
+		console.log(newNum);
+		$("#sakko" + num).clone(true).attr("id", "sakko" + newNum).appendTo(".kaljasakot-list");
+	
+	});
 });
-
-$(".kaljasakot-remove").click(function(){
-	count--;
-	$(".kaljasakot-sum").val(count);
-});
-
-})
