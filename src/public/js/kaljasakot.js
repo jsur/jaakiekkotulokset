81e431bc8a671651ff65970dc645446dc4189131
Julius Suominen <julius.suominen@gmail.com>
2016-10-09 20:17:01 +0300

    Counter disable/enable

--- src/public/js/kaljasakot.js
+++ src/public/js/kaljasakot.js
@@ -1,11 +1,16 @@
 $(function() {
 
-	
-
 	$(".kaljasakko-add").click(function(){
 
 		var index = $(this).parent().index() + 1;
 		var count = $("#kaljasakko-sum" + index).val();
+
+		console.log(count);
+
+		if (count + 1 > 0) {
+			$("#kaljasakko-subtract" + index).prop("disabled", false);
+		} 
+
 		count++;
 		$("#kaljasakko-sum" + index).val(count);
 	
@@ -15,9 +20,15 @@ $(function() {
 
 		var index = $(this).parent().index() + 1;
 		var count = $("#kaljasakko-sum" + index).val();
+		
+		if (count <= 0) {
+			$("#kaljasakko-subtract" + index).prop("disabled", true);
+		} else {
+
 		count--;
 		$("#kaljasakko-sum" + index).val(count);
-	
+		}
+
 	});
 
 	$(".kaljasakko-name").change(function(){
@@ -30,6 +41,6 @@ $(function() {
 		newElem.find(".kaljasakko-subtract").attr("id", "kaljasakko-subtract" + newNum);
 		newElem.find(".kaljasakko-sum").attr("id", "kaljasakko-sum" + newNum).val("");
 		newElem.find(".kaljasakko-add").attr("id", "kaljasakko-add" + newNum);
-	
+
 	});
 });
